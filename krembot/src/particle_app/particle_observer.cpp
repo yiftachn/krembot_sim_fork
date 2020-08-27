4bfb9982d3614830a448ade69bb9fd9b4401d7ce
eden_hartman <eden.r.hartman@gmail.com>
2020-08-27 15:36:05 +0300

    f

--- krembot/src/particle_app/particle_observer.cpp
+++ krembot/src/particle_app/particle_observer.cpp
@@ -51,7 +51,7 @@ void ParticleObserver::publish(const String & eventName, const String & content,
       }
     int milisec = millis();
     //3600000 milliseconds in an hour
-    int hours = sim_clock / 3600000;
+    int hours = milisec / 3600000;
     milisec = milisec - 3600000 * hours;
     //60000 milliseconds in a minute
     int min = milisec / 60000;
@@ -59,7 +59,7 @@ void ParticleObserver::publish(const String & eventName, const String & content,
     //1000 milliseconds in a second
     int sec = milisec / 1000;
     milisec = milisec - 1000 * sec;
-    
+
     char dateBuff[30];
     snprintf(dateBuff, sizeof(dateBuff), "2020-07-01T%02d:%02d:%02d.%d00Z", hours, min, sec, milisec);
     //the wanted format: 2019-04-10T13:15:52.087Z
